e4fd407b84cf781eae2359271fdb9535c9af178e
Edin Hadzovic <contact@edinhadzovic.com>
2024-11-21 21:41:27 +0100

    spelling mistake

--- web/assets/index-CYdfDbwP.js
+++ web/assets/index-CYdfDbwP.js
@@ -1,4 +1,4 @@
-import{T as $t,M as oe,R as N,E as k,C as $,a as X,b as Br,O as Z,c as ht}from"./index-CO6T2fQy.js";import{m as Wr}from"./index-BGCgPu5C.js";/**
+import{T as $t,M as oe,R as N,E as k,C as $,a as X,b as Br,O as Z,c as ht}from"./index-bkygmIsw.js";import{m as Wr}from"./index-DIMP9zip.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
