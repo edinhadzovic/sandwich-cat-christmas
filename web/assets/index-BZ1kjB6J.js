8dd81f703ac790bfccd155a1b28a8604b866d8c4
Edin Hadzovic <contact@edinhadzovic.com>
2024-12-01 12:39:48 +0100

    using mainnet and deployed contract.

--- web/assets/index-BZ1kjB6J.js
+++ web/assets/index-BZ1kjB6J.js
@@ -1,4 +1,4 @@
-import{T as $t,M as oe,R as N,E as k,C as $,a as X,b as Br,O as Z,c as ht}from"./index-BJ7HE8A-.js";import{m as Wr}from"./index-CroL-lSX.js";/**
+import{T as $t,M as oe,R as N,E as k,C as $,a as X,b as Br,O as Z,c as ht}from"./index-CjXIloM7.js";import{m as Wr}from"./index-Bzjbvts6.js";/**
  * @license
  * Copyright 2019 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
